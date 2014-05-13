88625f53801f64b29e3b6b874378b2ec0d6959c4
Keith M Corbett <keith@corbett.name>
2014-05-13 17:47:52 -0400

    tunnel script fixes

--- scripts/tunnel2code.sh
+++ scripts/tunnel2code.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # tunnel2code - get TCP/IP access over VPN to Verisk code server host CODE.VERISK.COM
-# Run this as root on Mac OS X or Linux to set up IP port tunnels over ssh.
-# Changes local hosts entry for CODE.VERISK.COM to redirect to VPN ssh server host.
-# Sets up ssh tunnels on ports 443 and 7999 to redirect to ssh server host.
+# Run this as root on Mac OS X or Linux to set up IP port tunnels over ssh
+# Changes local hosts entry for CODE.VERISK.COM to redirect to VPN ssh server host
+# Sets up ssh tunnels on ports 443 and 7999 to redirect to ssh server host
+# Authenticates to remote ssh using RSA private key in $HOME/.ssh/id_rsa if found 
 # Kill the script (^C) to tear down ssh tunnels and restore hosts table.
+
+set -u
 
 # All script command args are passed to ssh
 ARGS=$*
+
+# Code host is the server we want to reach for HTTPS and git/Stash
+CODEHOST_IP=10.16.91.42
 
 # Tunnel ports 443 (HTTPS) and 7999 (git/Stash)
 LOCAL_HTTPS_PORT=443
@@ -15,28 +21,27 @@ REMOTE_HTTPS_PORT=$LOCAL_HTTPS_PORT
 LOCAL_STASH_PORT=7999
 REMOTE_STASH_PORT=$LOCAL_STASH_PORT
 
-# VPN ssh server host must be accessible locally (presumably over VPN) and
+# VPN ssh server must be accessible locally (presumably over VPN) and
 #  must have access to the remote code host.
+#VPNSSHSERVER_IP=10.48.20.12
+#VPNSSHSERVER_USER=kcorbett
 VPNSSHSERVER_IP=192.168.67.119
 VPNSSHSERVER_USER=crucible
 
-CODEHOST_IP=10.16.91.42
-
+# Use RSA private key if found
+# Deliberately uses $HOME to find key for effective user before 'sudo'
 IDENT_FILE=$HOME/.ssh/id_rsa
-if [ -r $IDENT_FILE ]; then
-    ARGS="-i $IDENT_FILE $ARGS"
-fi
+[ -r $IDENT_FILE ] && ARGS="-i $IDENT_FILE $ARGS"
 
+# If this were a real program we would declare these variables
 TUNNEL1=
 TUNNEL2=
 LOCAL_REDIRECT_IP=
 IFCONFIG_UP=
 IFCONFIG_DOWN=
 
-set -u
-
 if [ ! -e "/private/etc/hosts" ] ; then
-    echo "On Unix"
+    echo "On Linux"
     LOCAL_REDIRECT_IP=$VPNSSHSERVER_IP
     TUNNEL1="$LOCAL_HTTPS_PORT:$CODEHOST_IP:$REMOTE_HTTPS_PORT"
     TUNNEL2="$LOCAL_STASH_PORT:$CODEHOST_IP:$REMOTE_STASH_PORT"
@@ -49,7 +54,7 @@ else
     LOCAL_IP2=172.16.123.2
     LOCAL_REDIRECT_IP=$LOCAL_IP2
     TUNNEL1="$LOCAL_IP1:$LOCAL_HTTPS_PORT:$CODEHOST_IP:$REMOTE_HTTPS_PORT"
-    TUNNEL2="$LOCAL_IP2:$LOCAL_STASH_PORT:$CODEHOST_IP:$REMOTE_STASH_PORT"
+    TUNNEL2="$LOCAL_IP1:$LOCAL_STASH_PORT:$CODEHOST_IP:$REMOTE_STASH_PORT"
     IFCONFIG_UP=
     IFCONFIG_DOWN=
     #
@@ -78,7 +83,8 @@ trap cleanup SIGHUP SIGINT SIGTERM
 echo "Tunneling..."
 
 $IFCONFIG_UP
-ssh -N $ARGS -L $TUNNEL1 -L $TUNNEL2 -l $VPNSSHSERVER_USER -N $VPNSSHSERVER_IP
+ARGS="$ARGS -L $TUNNEL1 -L $TUNNEL2 -l $VPNSSHSERVER_USER -N $VPNSSHSERVER_IP"
+ssh $ARGS
 
 echo "Interrupted."
 cleanup
